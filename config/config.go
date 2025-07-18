8ca918a50c735688d25286b11c8d3d85865f31e1
Michal Hanajik <michal@hanajik.com>
2025-07-18 23:42:07 +0200

    wip: basic auth

--- config/config.go
+++ config/config.go
@@ -8,6 +8,10 @@ type Config struct {
 	Domaine string `env:"SECRET_SERVICE_DOMAIN" envDefault:"http://localhost:8080"`
 	DB      string `env:"SECRET_SERVICE_DB" envDefault:"localhost:8081"`
 	Host    string `env:"SECRET_SERVICE_HOST" envDefault:"localhost:8080"`
+
+	BasicAuthEnabled  bool   `env:"SECRET_SERVICE_BASIC_AUTH_ENABLED" envDefault:"false"`
+	BasicAuthUsername string `env:"SECRET_SERVICE_BASIC_AUTH_USERNAME" envDefault:"admin"`
+	BasicAuthPassword string `env:"SECRET_SERVICE_BASIC_AUTH_PASSWORD" envDefault:"admin"`
 }
 
 func (c *Config) Load() error {
--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,6 +1,10 @@
 package api
 
 import (
+	"encoding/base64"
+	"net/http"
+	"strings"
+
 	"github.com/alexedwards/flow"
 	"github.com/pudottapommin/secret-notes/config"
 	"github.com/pudottapommin/secret-notes/pkg/secrets"
@@ -23,6 +27,39 @@ func NewHandlers(cfg *config.Config, client valkey.Client) *handlers {
 }
 
 func (h *handlers) AddHandlers(e *flow.Mux) {
-	e.HandleFunc("/create", h.secretsPOST, "POST")
+	e.Group(func(g *flow.Mux) {
+		if h.cfg.BasicAuthEnabled {
+			g.Use(func(next http.Handler) http.Handler {
+				return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					if r.URL.Path == "/create" && r.Method == "POST" {
+						authHeader := r.Header.Get("Authorization")
+						if authHeader == "" {
+							w.WriteHeader(http.StatusUnauthorized)
+							return
+						}
+
+						if !strings.HasPrefix(authHeader, "Basic ") {
+							w.WriteHeader(http.StatusUnauthorized)
+							return
+						}
+
+						payload, err := base64.StdEncoding.DecodeString(authHeader[6:])
+						if err != nil {
+							w.WriteHeader(http.StatusUnauthorized)
+							return
+						}
+
+						pair := strings.SplitN(string(payload), ":", 2)
+						if len(pair) != 2 || pair[0] != h.cfg.BasicAuthUsername || pair[1] != h.cfg.BasicAuthPassword {
+							w.WriteHeader(http.StatusUnauthorized)
+							return
+						}
+					}
+					next.ServeHTTP(w, r)
+				})
+			})
+		}
+		g.HandleFunc("/create", h.secretsPOST, "POST")
+	})
 	e.HandleFunc("/:slug", h.secretsGET, "GET")
 }
